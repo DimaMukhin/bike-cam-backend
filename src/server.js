d8420b8fa686ac6121462d5096bd3e70e241f09c
Dima Mukhin <dima9348@gmail.com>
2018-09-08 12:04:23 -0700

    initial commit

---
+++ src/server.js
@@ -0,0 +1,33 @@
+const Koa = require('koa');
+const Router = require('koa-router');
+const Logger = require('koa-logger');
+const Cors = require('@koa/cors');
+const BodyParser = require('koa-bodyparser');
+const Helmet = require('koa-helmet');
+
+const port = process.env.PORT || 3000
+const app = new Koa();
+const router = new Router();
+
+app.use(Helmet());
+
+if (process.env.NODE_ENV === 'development') {
+    app.use(Logger());
+}
+
+app.use(Cors());
+app.use(BodyParser({
+    enableTypes: ['json'],
+    jsonLimit: '5mb',
+    strict: true,
+    onerror: function (err, ctx) {
+        ctx.throw('body parse error', 422)
+    }
+}));
+
+// API routes
+require('./routes')(router);
+app.use(router.routes());
+app.use(router.allowedMethods());
+
+app.listen(port, () => console.log(`API server started on ${port}`));
